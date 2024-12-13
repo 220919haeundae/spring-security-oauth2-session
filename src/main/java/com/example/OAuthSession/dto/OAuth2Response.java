aa1a410a89ceee523a79ae9cccd474e69cd39de8
220919haeundae <113912025+220919haeundae@users.noreply.github.com>
2024-12-13 19:20:05 +0900

    feat: spring security oauth2 네이버, 구글 소셜 로그인 기능 추가  - DefaultOAuth2UserService 상속 클래스 구현  - 네이버 및 구글에서 반환한 UserInfo 담을 OAuth2Response(UserDetails 역할) 인터페이스 및 구현 클래스 추가  - SecurityConfig oauth2Login에 CustomOAuthUserService 설정 추가

---
+++ src/main/java/com/example/OAuthSession/dto/OAuth2Response.java
@@ -0,0 +1,13 @@
+package com.example.OAuthSession.dto;
+
+public interface OAuth2Response {
+
+    String getProvider();
+
+    String getProviderId();
+
+    String getEmail();
+
+    String getName();
+
+}
